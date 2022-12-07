d9b4931c4f65c038cf488e47571d5de85f9c8bca
abdullah-aladham <abdullah.aladham@gmail.com>
2022-12-07 14:41:18 +0200

    concat. problem solved

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-# firstname="Abdullah\t"
-# lastname="Aladham"
-# print(firstname+lastname)
-a="first \
-    second\
-    third "
-b= "A\
-    B\
-    C"
-print(a+" "+b) 
+#you have a set of characters you need to make a word from them
+#using concat and print the string on screen.
+#example : char1=d ,char2=o char3=o,char4=r
+
+#solution
+char1='d'
+char2='o'
+char3='o'
+char4='r'
+str=char1+char2+char3+char4
+print(str)#door
+
+#my linkedin: https://www.linkedin.com/in/abdullah-aladham/
+#my github: https://github.com/abdullah-aladham
+
+
+
