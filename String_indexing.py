a95ed7f1374296d2760b11cf98231a538c179768
abdullah-aladham <abdullah.aladham@gmail.com>
2022-12-10 11:28:59 +0200

    Notes for string and indexing

---
+++ String_indexing.py
@@ -0,0 +1,7 @@
+#STRING Indexing & Slicing 
+# [1] all data in python is object 
+# [2] Object Contain elements
+# [3] Every element has its own index
+# [4] Python use zero Based Indexing (Index starts from zero)
+# [5] use square brackets to access elements
+# [6] Enable Accessing Parts of strings , tuples or Lists 
---
+++ string_problem.py
@@ -0,0 +1,13 @@
+single='this is single quote str'
+doubleq="this is double quote str"
+doubleinSingle='this is "double" quote in single quote'
+
+print(single+"\t"+doubleq+"\t"+doubleinSingle)
+
+dlstr="""first
+second '1st intruder string' "second intruder str" 
+third"""
+bsstr="this is str1\b"
+print(dlstr)
+print("\n")
+print(bsstr)
